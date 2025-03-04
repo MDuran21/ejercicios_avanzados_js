488dba50a021c9d5af14d00246b6e9749daccf26
MDuran21 <ciber.mduran@gmail.com>
2025-03-04 01:15:58 +0100

    Second commit

--- ejercicio4.js
+++ ejercicio4.js
@@ -1,3 +1,4 @@
+//4.1
 const mainCharacters = [
   'Luke',
   'Leia',
@@ -7,21 +8,33 @@ const mainCharacters = [
   'Anakin',
   'Obi-Wan'
 ]
-
-findArrayIndex(mainCharacters, 'Luke')
+let index = undefined
+let found = false
+findArrayIndex(mainCharacters, 'Anakin')
 
 function findArrayIndex(array, text) {
-  let found = false
   for (let position of array) {
     if (position === text) {
       found = true
     }
   }
   if (found === true) {
+    index = array.indexOf(text)
     console.log(
-      `${text} is in the position ${array.position.IndexOf(text)} of the array`
+      `${text} is in the position ${array.indexOf(text)} of the array`
     )
   } else {
     console.log(`${text} is not in the array`)
   }
 }
+//4.2
+if (found === true) {
+  removeItem(mainCharacters, index)
+}
+
+function removeItem(array, index) {
+  array.splice(index, 1)
+  console.log(array)
+}
+
+//Para poder quitar 3 nombres a la vez solo haría falta juntar en una funcion las dos anteriores y que se ejecuten secuencialmente con los nombres que quieres eliminar
---
+++ ejercicio5.js
@@ -0,0 +1,4 @@
+function rollDice() {}
+let selection = 8
+const random = Math.random() * selection
+console.log(random)
